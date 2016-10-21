c00f2f756b6208fedc826d408079667573676ff9
Kominam <kominam.2511@gmail.com>
2016-10-21 13:23:40 +0700

    Finish user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by id: params[:id]
+    if @user.nil?
+      render file: "public/404.html", status: 404, layout: false
+    end
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".signup_success"
+      redirect_to @user
+    else
+      flash[:danger] = t ".signup_fail"
+      render :new
+    end
+  end
+
+  private def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
